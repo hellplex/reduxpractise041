3af4993b5a19c25a9cc2ce0144801dc9729f7319
Cristina Suarez <csuarezcorzo@gmail.com>
2018-06-03 19:11:11 +0200

    Complete the book app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascrip: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 210 },
+        { title: 'The Dark Tower', pages: 390 },
+        { title: 'Eloquent Ruby', pages: 1 },
+    ]
+}
